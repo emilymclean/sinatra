530818e9312537059dade6ae7b424c6c464ecb35
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-07 22:07:39 +1100

    WIP

--- shared/src/commonMain/kotlin/cl/emilym/sinatra/Extensions.kt
+++ shared/src/commonMain/kotlin/cl/emilym/sinatra/Extensions.kt
@@ -5,3 +5,6 @@ import io.github.aakira.napier.Napier
 fun Napier.e(throwable: Throwable) {
     e(throwable.message ?: "Exception", throwable)
 }
+
+val Float.deg
+    get() = this * 0.01745f
--- ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/widgets/RouteLine.kt
+++ ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/widgets/RouteLine.kt
@@ -15,16 +15,29 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.layout.MultiContentMeasurePolicy
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import cl.emilym.compose.units.rdp
 import cl.emilym.sinatra.data.models.Route
 import cl.emilym.sinatra.data.models.Stop
+import cl.emilym.sinatra.deg
 import cl.emilym.sinatra.ui.color
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.sin
 
 @Composable
 fun RouteNode(
@@ -56,29 +69,106 @@ fun RouteLine(
     stops: List<Stop>
 ) {
     val color = route.colors?.color() ?: MaterialTheme.colorScheme.onSurface
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .horizontalScroll(rememberScrollState())
-            .width(IntrinsicSize.Max),
-    ) {
-        Box(Modifier
-            .height(0.5.rdp)
-            .padding(horizontal = 1.5.rdp)
-            .fillMaxWidth()
-            .background(color)
-        )
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(1.rdp),
-        ) {
-            Box {}
-            for (i in stops.indices) {
-                RouteNode(
-                    i == 0 || i == stops.size - 1,
-                    color
+    Box(Modifier.horizontalScroll(rememberScrollState())) {
+        RouteLine(
+            {
+                for (i in stops.indices) {
+                    RouteNode(
+                        i == 0 || i == stops.size - 1,
+                        color
+                    )
+                }
+            },
+            {
+                Text(route.name, style = MaterialTheme.typography.labelMedium)
+            },
+            {
+                Box(
+                    Modifier
+                        .background(color)
+                        .height(0.5.rdp)
+                        .fillMaxWidth()
                 )
             }
-            Box {}
+        )
+    }
+}
+
+@Composable
+internal fun RouteLine(
+    stopNodes: @Composable () -> Unit,
+    stopText: @Composable () -> Unit,
+    line: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Layout(
+        contents = listOf(stopNodes, stopText, line),
+        modifier = modifier,
+        measurePolicy = with(LocalDensity.current) {
+            RouteLineMeasurePolicy(
+                1.rdp.toPx().toInt(),
+                1.rdp.toPx().toInt(),
+                0.25.rdp.toPx().toInt()
+            )
+        }
+    )
+}
+
+class RouteLineMeasurePolicy(
+    val horizontalMargin: Int,
+    val spaceBetweenNodes: Int,
+    val spaceBetweenNodeAndText: Int,
+    textRotation: Float = -80f,
+): MultiContentMeasurePolicy {
+
+    val textRotation = textRotation.deg
+
+    override fun MeasureScope.measure(
+        measurables: List<List<Measurable>>,
+        constraints: Constraints
+    ): MeasureResult {
+        val lineOffset = 5
+
+        val stopNodeMeasurables = measurables[0]
+        val stopTextMeasurables = measurables[1]
+        val lineMeasurable = measurables[2][0]
+
+        val stopText = stopTextMeasurables.map { it.measure(constraints) }
+        val stopNode = stopNodeMeasurables.map { it.measure(constraints) }
+
+        val maxTextHeight = stopText.maxOf {
+            abs(it.width * sin(textRotation)) + abs(it.height * cos(textRotation))
+        }
+        val totalTextWidth = stopText.fold(0f) { acc,it ->
+            acc + abs(it.width * cos(textRotation)) + abs(it.height * sin(textRotation))
+        }
+        val totalNodeWidth = stopNode.fold(0f) { acc, it ->
+            acc + it.width
+        }
+
+        val nodeHeight = stopNode[0].height
+        val nodeWidth = stopNode[0].width
+        val width = (
+                max(totalTextWidth, totalNodeWidth) +
+                (spaceBetweenNodes * stopNodeMeasurables.lastIndex)
+        ).toInt()
+        val height = (maxTextHeight + stopNode[0].height + spaceBetweenNodeAndText).toInt()
+
+        val line = lineMeasurable.measure(constraints.copy(minWidth = width - (lineOffset * 2), maxWidth = width - (lineOffset * 2)))
+
+        return layout(width + (horizontalMargin * 2), height) {
+            val nodeY = height - (nodeHeight / 2)
+            line.place(horizontalMargin + lineOffset, nodeY + (line.height/2))
+            for (i in stopNode.indices) {
+                val x = when {
+                    i == 0 -> horizontalMargin
+                    else -> i * (nodeWidth + spaceBetweenNodes) + horizontalMargin
+                }
+                stopNode[i].place(
+                    x,
+                    nodeY,
+                )
+            }
         }
     }
 }
