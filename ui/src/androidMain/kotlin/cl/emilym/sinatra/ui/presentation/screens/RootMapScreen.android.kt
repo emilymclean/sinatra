f755e5797481ea7d4816f73303943faf4347ec56
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-09 21:50:47 +1100

    Pad google icon so it is visible in peek state

--- ui/src/androidMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/RootMapScreen.android.kt
+++ ui/src/androidMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/RootMapScreen.android.kt
@@ -1,15 +1,27 @@
 package cl.emilym.sinatra.ui.presentation.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.dp
 import cl.emilym.sinatra.ui.canberra
 import cl.emilym.sinatra.ui.navigation.CurrentMapContent
+import cl.emilym.sinatra.ui.navigation.LocalBottomSheetState
 import cl.emilym.sinatra.ui.navigation.MapScope
 import cl.emilym.sinatra.ui.navigation.bottomSheetHalfHeight
 import cl.emilym.sinatra.ui.toMaps
+import cl.emilym.sinatra.ui.widgets.bottomsheet.NotShitSheetValue
+import cl.emilym.sinatra.ui.widgets.screenHeight
 import cl.emilym.sinatra.ui.widgets.screenSize
+import cl.emilym.sinatra.ui.widgets.toDp
 import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
@@ -21,6 +33,8 @@ actual fun Map() {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(canberra.toMaps(), 10f)
     }
+    val windowPadding = ScaffoldDefaults.contentWindowInsets.asPaddingValues()
+    val layoutDirection = LocalLayoutDirection.current
 
     Box {
         GoogleMap(
@@ -34,10 +48,14 @@ actual fun Map() {
                 compassEnabled = false,
                 myLocationButtonEnabled = false,
                 zoomControlsEnabled = false
+            ),
+            contentPadding = PaddingValues(
+                windowPadding.calculateStartPadding(layoutDirection),
+                windowPadding.calculateTopPadding(),
+                windowPadding.calculateEndPadding(layoutDirection),
+                windowPadding.calculateBottomPadding() + 56.dp
             )
         ) {
-
-
             MapScope(
                 cameraPositionState,
                 screenSize(),
