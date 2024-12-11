4de9a89c5f29621f4a5103b50333b4159cd39052
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-12 06:56:00 +1100

    Setup mapkit

--- ui/src/iosMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/RootMapScreen.ios.kt
+++ ui/src/iosMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/RootMapScreen.ios.kt
@@ -1,15 +1,19 @@
 package cl.emilym.sinatra.ui.presentation.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitView
 import cafe.adriel.voyager.core.screen.Screen
 import cl.emilym.sinatra.data.models.Bounds
 import cl.emilym.sinatra.data.models.Location
 import cl.emilym.sinatra.ui.navigation.MapControl
 import cl.emilym.sinatra.ui.navigation.MapScope
+import platform.MapKit.MKMapView
 
 @Composable
 actual fun Map(content: @Composable MapControl.(@Composable () -> Unit) -> Unit) {
+
     val control = object : MapControl {
         override fun zoomToArea(bounds: Bounds, padding: Int) {}
 
@@ -17,5 +21,18 @@ actual fun Map(content: @Composable MapControl.(@Composable () -> Unit) -> Unit)
 
         override fun zoomToPoint(location: Location, zoom: Float) {}
     }
-    control.content {}
+    control.content {
+        UIKitView(
+            modifier = Modifier.fillMaxSize(),
+            factory = {
+                MKMapView().apply {
+                    setZoomEnabled(true)
+                    setScrollEnabled(true)
+                }
+            },
+            update = { mapView ->
+//                mapView.setCenterCoordinate(clLocation, animated = true)
+            }
+        )
+    }
 }
