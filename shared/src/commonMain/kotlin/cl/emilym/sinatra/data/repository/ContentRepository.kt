d71f761b2d6f8cfdb4f397ee2436d9f820f792b8
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-14 16:31:50 +1100

    Cache content in memory

--- shared/src/commonMain/kotlin/cl/emilym/sinatra/data/repository/ContentRepository.kt
+++ shared/src/commonMain/kotlin/cl/emilym/sinatra/data/repository/ContentRepository.kt
@@ -3,6 +3,7 @@ package cl.emilym.sinatra.data.repository
 import cl.emilym.sinatra.data.models.Content
 import cl.emilym.sinatra.data.models.ContentId
 import cl.emilym.sinatra.data.models.ContentLink
+import cl.emilym.sinatra.data.persistence.ContentPersistence
 import cl.emilym.sinatra.network.GtfsApi
 import org.koin.core.annotation.Factory
 
@@ -10,7 +11,8 @@ import org.koin.core.annotation.Factory
 class ContentRepository(
     private val remoteConfigRepository: RemoteConfigRepository,
     // Shouldn't be accessing this directly, but it's kinda a quick things so it's fine
-    private val gtfsApi: GtfsApi
+    private val gtfsApi: GtfsApi,
+    private val contentPersistence: ContentPersistence
 ) {
 
     companion object {
@@ -18,6 +20,8 @@ class ContentRepository(
     }
 
     private suspend fun aboutUsContent(): Content? {
+        contentPersistence.get(ABOUT_ID)?.let { return it }
+
         val contentUrl = remoteConfigRepository.aboutContentUrl() ?: return null
         val termsUrl = remoteConfigRepository.termsUrl()
         val privacyUrl = remoteConfigRepository.privacyPolicyUrl()
@@ -30,7 +34,9 @@ class ContentRepository(
                 ContentLink.external("Terms & Conditions", termsUrl),
                 ContentLink.external("Privacy Policy", privacyUrl)
             )
-        )
+        ).also {
+            contentPersistence.store(ABOUT_ID, it)
+        }
     }
 
     suspend fun content(id: ContentId): Content? {
