26c9ec72069a9213fbfacef3bae3d4a3c26f1da5
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-19 13:15:23 +1100

    Restructure remote config

--- shared/src/commonMain/kotlin/cl/emilym/sinatra/data/repository/RemoteConfigRepository.kt
+++ shared/src/commonMain/kotlin/cl/emilym/sinatra/data/repository/RemoteConfigRepository.kt
@@ -10,21 +10,28 @@ class RemoteConfigRepository(
     private val remoteConfigClient: RemoteConfigClient
 ) {
 
+    companion object {
+        const val API_URL_KEY = "api_url"
+        const val PRIVACY_POLICY_URL_KEY = "privacy_policy_url"
+        const val TERMS_URL_KEY = "terms_url"
+        const val ABOUT_CONTENT_URL_KEY = "about_content_url"
+    }
+
     @Throws(NoApiUrlException::class, CancellationException::class)
     suspend fun apiUrl(): String {
-        return remoteConfigClient.apiUrl()
+        return remoteConfigClient.string(API_URL_KEY) ?: throw NoApiUrlException()
     }
 
     suspend fun privacyPolicyUrl(): String? {
-        return remoteConfigClient.privacyPolicyUrl()
+        return remoteConfigClient.string(PRIVACY_POLICY_URL_KEY)
     }
 
     suspend fun termsUrl(): String? {
-        return remoteConfigClient.termsUrl()
+        return remoteConfigClient.string(TERMS_URL_KEY)
     }
 
     suspend fun aboutContentUrl(): String? {
-        return remoteConfigClient.aboutContentUrl()
+        return remoteConfigClient.string(ABOUT_CONTENT_URL_KEY)
     }
 
 }
