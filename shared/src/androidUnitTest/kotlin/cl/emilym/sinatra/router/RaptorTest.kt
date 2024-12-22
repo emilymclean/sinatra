8a9936473bfe102483c6950d3016c7cfb3efd924
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-22 19:51:57 +1100

    Add more tests

--- shared/src/androidUnitTest/kotlin/cl/emilym/sinatra/router/RaptorTest.kt
+++ shared/src/androidUnitTest/kotlin/cl/emilym/sinatra/router/RaptorTest.kt
@@ -10,18 +10,17 @@ import pbandk.decodeFromByteArray
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
 import kotlin.time.Duration
 
 class RaptorTest {
 
     companion object {
-        const val STOP_ID_SWINDEN_STREET_ALG = "8118"
         const val STOP_ID_SWINDEN_STREET_GGN = "8119"
         const val STOP_ID_SWINDEN_STREET = "SWN"
-        const val STOP_ID_MANNING_CLARK_ALG = "8104"
         const val STOP_ID_MANNING_CLARK_GGN = "8105"
         const val STOP_ID_MANNING_CLARK = "MCK"
-        const val START_OF_DAY = 1734786000 // 2024-12-22 00:00:00 AEDT
+        const val STOP_ID_CANBERRA_RAILWAY_STATION = "3320"
     }
 
     lateinit var graph: Graph
@@ -62,7 +61,7 @@ class RaptorTest {
                 STOP_ID_MANNING_CLARK
             )
         } catch(e: RouterException) { null }
-        assertEquals(result, Journey(
+        assertEquals(Journey(
             stops = listOf("SWN", "8119", "8105", "MCK"),
             connections = listOf(
                 JourneyConnection.Transfer(
@@ -76,7 +75,53 @@ class RaptorTest {
                 ),
                 JourneyConnection.Transfer(travelTime = 9)
             )
+        ), result)
+    }
+
+    @Test
+    fun testValidJourneyAcrossMultipleRoutes() {
+        val result = raptor.calculate(
+            Duration.parseIsoString("PT09H").inWholeSeconds,
+            STOP_ID_CANBERRA_RAILWAY_STATION,
+            STOP_ID_MANNING_CLARK
+        )
+        assertEquals(result, Journey(
+            stops = listOf("3320", "2232", "3406", "8129", "8105", "MCK"),
+            connections = listOf(
+                JourneyConnection.Transfer(
+                    travelTime = 1040
+                ),
+                JourneyConnection.Travel(
+                    routeId = "6-10647",
+                    heading = "City ANU",
+                    startTime = 33480L,
+                    endTime = 37080L
+                ),
+                JourneyConnection.Transfer(
+                    travelTime = 109
+                ),
+                JourneyConnection.Travel(
+                    routeId = "ACTO001",
+                    heading = "Gungahlin Pl",
+                    startTime = 37200L,
+                    endTime = 38557
+                ),
+                JourneyConnection.Transfer(
+                    travelTime = 9
+                )
+            )
         ))
+    }
+
+    @Test
+    fun testInvalidJourney() {
+        assertFails {
+            raptor.calculate(
+                Duration.parseIsoString("PT23H").inWholeSeconds,
+                STOP_ID_CANBERRA_RAILWAY_STATION,
+                STOP_ID_MANNING_CLARK
+            )
+        }
     }
 
 }
