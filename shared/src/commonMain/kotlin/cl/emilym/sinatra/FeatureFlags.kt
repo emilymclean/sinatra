a56183858ccb549437bccd744d2d849b511e87e4
Emily McLean <emilymcleandeveloper@gmail.com>
2024-12-31 12:24:09 +1100

    Merge branch 'develop' into proper-routing

--- shared/src/commonMain/kotlin/cl/emilym/sinatra/FeatureFlags.kt
+++ shared/src/commonMain/kotlin/cl/emilym/sinatra/FeatureFlags.kt
@@ -3,7 +3,7 @@ package cl.emilym.sinatra
 object FeatureFlags {
     const val ROUTE_DETAIL_CLICKABLE_STOPS = true
     const val ROUTE_DETAIL_HIGHLIGHT_SOURCE_STOP = true
-    const val ROUTE_DETAIL_PREVENT_ZOOM_WHEN_HAVE_SOURCE_STOP = true
+    const val ROUTE_DETAIL_PREVENT_ZOOM_WHEN_HAVE_SOURCE_STOP = false
     const val ROUTE_DETAIL_NEAREST_STOP = true
     const val STOP_DETAIL_SHOW_IN_MAPS_BUTTON = false
     const val MAP_SEARCH_SCREEN_NEARBY_STOPS_SEARCH = true
