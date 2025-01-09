b98545325d8d3771b68b32846fc263260888f22a
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-09 23:25:08 +1100

    Add all screens into a module

--- ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/Routes.kt
+++ ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/Routes.kt
@@ -3,16 +3,44 @@ package cl.emilym.sinatra.ui
 import cafe.adriel.voyager.core.registry.ScreenProvider
 import cafe.adriel.voyager.navigator.Navigator
 import cl.emilym.kmp.serializable.Serializable
+import cl.emilym.sinatra.data.models.ContentId
 import cl.emilym.sinatra.data.models.Place
 import cl.emilym.sinatra.data.models.Route
+import cl.emilym.sinatra.data.models.RouteId
+import cl.emilym.sinatra.data.models.ServiceId
 import cl.emilym.sinatra.data.models.Stop
+import cl.emilym.sinatra.data.models.StopId
+import cl.emilym.sinatra.data.models.TripId
 import cl.emilym.sinatra.ui.presentation.screens.maps.RouteDetailScreen
 import cl.emilym.sinatra.ui.presentation.screens.maps.StopDetailScreen
 import cl.emilym.sinatra.ui.presentation.screens.maps.navigate.NavigateEntryScreen
 import cl.emilym.sinatra.ui.presentation.screens.maps.navigate.NavigationLocation
+import kotlinx.datetime.Instant
 
 sealed interface ScreenRoute: ScreenProvider, Serializable {
-    data object HomeScreen: ScreenRoute
+    data object MapSearch: ScreenRoute
+    data class Content(
+        val id: ContentId
+    ): ScreenRoute
+    data object About: ScreenRoute
+    data object Favourite: ScreenRoute
+    data class RouteDetail(
+        val routeId: RouteId
+    ): ScreenRoute
+    data class TripDetail(
+        val routeId: RouteId,
+        val serviceId: ServiceId,
+        val tripId: TripId,
+        val stopId: StopId,
+        val startOfDay: Instant
+    ): ScreenRoute
+    data class StopDetail(
+        val stopId: StopId
+    ): ScreenRoute
+    data class NavigationEntry(
+        val destination: NavigationLocation,
+        val origin: NavigationLocation? = null
+    ): ScreenRoute
 }
 
 fun Navigator.placeCardDefaultNavigation(place: Place) {
--- ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/presentation/ScreenModule.kt
+++ ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/presentation/ScreenModule.kt
@@ -1,11 +1,53 @@
 package cl.emilym.sinatra.ui.presentation
 
 import cafe.adriel.voyager.core.registry.screenModule
+import cl.emilym.sinatra.data.repository.ContentRepository
 import cl.emilym.sinatra.ui.ScreenRoute
-import cl.emilym.sinatra.ui.presentation.screens.HomeScreen
+import cl.emilym.sinatra.ui.presentation.screens.AboutScreen
+import cl.emilym.sinatra.ui.presentation.screens.ContentScreen
+import cl.emilym.sinatra.ui.presentation.screens.FavouriteScreen
+import cl.emilym.sinatra.ui.presentation.screens.maps.RouteDetailScreen
+import cl.emilym.sinatra.ui.presentation.screens.maps.StopDetailScreen
+import cl.emilym.sinatra.ui.presentation.screens.maps.navigate.NavigateEntryScreen
+import cl.emilym.sinatra.ui.presentation.screens.maps.search.MapSearchScreen
 
 val sharedScreenModule = screenModule {
-    register<ScreenRoute.HomeScreen> {
-        HomeScreen()
+    register<ScreenRoute.MapSearch> {
+        MapSearchScreen()
+    }
+    register<ScreenRoute.About> {
+        AboutScreen()
+    }
+    register<ScreenRoute.Favourite> {
+        FavouriteScreen()
+    }
+    register<ScreenRoute.Content> {
+        when (it.id) {
+            ContentRepository.ABOUT_ID -> AboutScreen()
+            else -> ContentScreen(it.id)
+        }
+    }
+    register<ScreenRoute.RouteDetail> {
+        RouteDetailScreen(it.routeId)
+    }
+    register<ScreenRoute.TripDetail> {
+        RouteDetailScreen(
+            it.routeId,
+            it.serviceId,
+            it.tripId,
+            it.stopId,
+            it.startOfDay
+        )
+    }
+    register<ScreenRoute.StopDetail> {
+        StopDetailScreen(
+            it.stopId
+        )
+    }
+    register<ScreenRoute.NavigationEntry> {
+        NavigateEntryScreen(
+            it.destination,
+            it.origin
+        )
     }
 }
