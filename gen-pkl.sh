2e7678b6816280eac4b14e28567214e49631cb64
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-09 20:50:15 +1100

    Fix lint issues

--- gen-pkl.sh
+++ gen-pkl.sh
@@ -1,2 +1,3 @@
 pkl eval .github/workflows/build.pkl -f yaml -o .github/workflows/build.yml
 pkl eval .github/workflows/test.pkl -f yaml -o .github/workflows/test.yml
+pkl eval .github/workflows/lint.pkl -f yaml -o .github/workflows/lint.yml
