87f7bc822a59189ac65cae252f8973193331808c
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-14 18:03:42 +1100

    Never report using 24 hour time on iOS
    Check appears broken (why isn't there an API for checking this setting?)

--- ui/src/iosMain/kotlin/cl/emilym/sinatra/ui/localization/TimeHelpers.ios.kt
+++ ui/src/iosMain/kotlin/cl/emilym/sinatra/ui/localization/TimeHelpers.ios.kt
@@ -8,9 +8,10 @@ import platform.Foundation.NSDateFormatterShortStyle
 
 @Composable
 actual fun is24HourTimeFormat(): Boolean {
-    val formatter = NSDateFormatter()
-    formatter.setDateStyle(NSDateFormatterNoStyle)
-    formatter.setTimeStyle(NSDateFormatterShortStyle)
-    val dateString = formatter.stringFromDate(NSDate())
-    return !dateString.contains("AM") && !dateString.contains("PM")
+//    val formatter = NSDateFormatter()
+//    formatter.setDateStyle(NSDateFormatterNoStyle)
+//    formatter.setTimeStyle(NSDateFormatterShortStyle)
+//    val dateString = formatter.stringFromDate(NSDate())
+//    return !dateString.contains("AM") && !dateString.contains("PM")
+    return false
 }
