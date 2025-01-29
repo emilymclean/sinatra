83b4be133dad2d0cfe127f6d6534b8df80efe37f
Emily McLean <emilymcleandeveloper@gmail.com>
2025-01-29 17:39:40 +1100

    Fix recursive function definition in iOS implementation

--- ui/src/iosMain/kotlin/cl/emilym/sinatra/ui/widgets/FlowHelpers.ios.kt
+++ ui/src/iosMain/kotlin/cl/emilym/sinatra/ui/widgets/FlowHelpers.ios.kt
@@ -2,10 +2,11 @@ package cl.emilym.sinatra.ui.widgets
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import cl.emilym.sinatra.ui.widgets.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.StateFlow
 import kotlin.coroutines.CoroutineContext
 
 @Composable
 actual fun <T> StateFlow<T>.collectAsStateWithLifecycle(context: CoroutineContext): State<T> =
-    collectAsStateWithLifecycle(context)
+    collectAsState(context)
