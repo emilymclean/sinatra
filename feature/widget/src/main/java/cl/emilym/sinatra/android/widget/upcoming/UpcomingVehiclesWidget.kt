9fc3484d9281d7a99fdb1cf8fb173be0aa80e7a4
Emily McLean <emilymcleandeveloper@gmail.com>
2025-04-17 22:17:33 +1000

    Add error state

--- feature/widget/src/main/java/cl/emilym/sinatra/android/widget/upcoming/UpcomingVehiclesWidget.kt
+++ feature/widget/src/main/java/cl/emilym/sinatra/android/widget/upcoming/UpcomingVehiclesWidget.kt
@@ -1,21 +1,27 @@
 package cl.emilym.sinatra.android.widget.upcoming
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.glance.Button
 import androidx.glance.GlanceComposable
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.LocalContext
+import androidx.glance.LocalGlanceId
 import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.state.GlanceStateDefinition
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
@@ -27,6 +33,8 @@ import cl.emilym.sinatra.android.widget.data.proto.UpcomingType
 import cl.emilym.sinatra.android.widget.data.proto.UpcomingVehicleState
 import cl.emilym.sinatra.android.widget.data.proto.UpcomingVehicleStopTime
 import cl.emilym.sinatra.nullIfBlank
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Instant
 
 class UpcomingVehiclesWidget: SinatraGlanceAppWidget() {
@@ -45,6 +53,41 @@ class UpcomingVehiclesWidget: SinatraGlanceAppWidget() {
                 contentAlignment = Alignment.Center
             ) {
                 when {
+                    state.errorMessage.isNotBlank() -> {
+                        val context = LocalContext.current
+                        val glanceId = LocalGlanceId.current
+                        Column {
+                            Text(
+                                LocalContext.current.getString(
+                                    R.string.upcoming_vehicle_widget_something_went_wrong,
+                                ),
+                                style = TextStyle(
+                                    color = GlanceTheme.colors.onSurface,
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Bold
+                                ),
+                                modifier = GlanceModifier.fillMaxWidth()
+                            )
+                            Text(
+                                state.errorMessage ?: "",
+                                style = TextStyle(
+                                    color = GlanceTheme.colors.onSurface,
+                                    textAlign = TextAlign.Center
+                                ),
+                                modifier = GlanceModifier.fillMaxWidth()
+                            )
+                            Spacer(GlanceModifier.height(8.dp))
+                            Button(
+                                LocalContext.current.getString(
+                                    R.string.upcoming_vehicle_widget_retry,
+                                ),
+                                {
+                                    MainScope().launch { update(context, glanceId) }
+                                },
+                                modifier = GlanceModifier.fillMaxWidth()
+                            )
+                        }
+                    }
                     !state.hasUpcoming -> {
                         Text(
                             LocalContext.current.getString(
