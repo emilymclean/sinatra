b76c4df310e03fd8920935e418480e3758e0fbea
Emily McLean <emilymcleandeveloper@gmail.com>
2025-05-31 16:07:33 +1000

    Add backing to cards to allow changing icon

--- ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/widgets/StopCard.kt
+++ ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/widgets/StopCard.kt
@@ -65,23 +65,24 @@ fun TimetableStationTime.pick(route: Route? = null): StopStationTime {
 }
 
 @Composable
-fun StopCard(
+fun DefaultStopCardIcon(stop: Stop) {
+    BusIcon()
+}
+
+@Composable
+fun IconStopCard(
     stop: Stop,
     modifier: Modifier = Modifier,
     stopStationTime: StopStationTime? = null,
     onClick: () -> Unit,
     subtitle: String? = null,
-    showStopIcon: Boolean = false,
+    icon: (@Composable () -> Unit)?
 ) {
     val stopListingSemantics = stringResource(Res.string.semantics_stop_listing, stop.name)
     ListCard(
-        if (showStopIcon) {
-            {
-                RandleScaffold {
-                    BusIcon()
-                }
-            }
-        } else null,
+        icon?.let {
+            { RandleScaffold { it() } }
+        },
         Modifier
             .semantics {
                 contentDescription = stopListingSemantics
@@ -122,6 +123,28 @@ fun StopCard(
             )
         }
     }
+}
+
+@Composable
+fun StopCard(
+    stop: Stop,
+    modifier: Modifier = Modifier,
+    stopStationTime: StopStationTime? = null,
+    onClick: () -> Unit,
+    subtitle: String? = null,
+    showStopIcon: Boolean = false,
+) {
+    IconStopCard(
+        stop,
+        modifier,
+        stopStationTime,
+        onClick,
+        subtitle,
+        when (showStopIcon) {
+            true -> { { DefaultStopCardIcon(stop) } }
+            else -> null
+        }
+    )
 }
 
 
