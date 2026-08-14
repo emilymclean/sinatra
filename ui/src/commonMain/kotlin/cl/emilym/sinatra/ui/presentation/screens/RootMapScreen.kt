2cb6b11ae8b703ae43e6d0eb4aeca506faed76a6
Emily McLean <emilymcleandeveloper@gmail.com>
2026-08-14 17:28:38 +1000

    Show back button over map when bottom sheet not visible

--- ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/RootMapScreen.kt
+++ ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/RootMapScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -38,19 +39,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.invisibleToUser
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
 import androidx.window.core.layout.WindowWidthSizeClass
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.CurrentScreen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import cl.emilym.compose.units.px
 import cl.emilym.sinatra.FeatureFlag
+import cl.emilym.sinatra.data.repository.isIos
 import cl.emilym.sinatra.ui.maps.MapControl
 import cl.emilym.sinatra.ui.maps.rememberMapControl
 import cl.emilym.sinatra.ui.navigation.CurrentBottomSheetContent
@@ -63,6 +70,7 @@ import cl.emilym.sinatra.ui.plus
 import cl.emilym.sinatra.ui.presentation.screens.content.MoreScreen
 import cl.emilym.sinatra.ui.presentation.screens.maps.navigate.NavigateEntryScreen
 import cl.emilym.sinatra.ui.presentation.screens.maps.search.MapSearchScreen
+import cl.emilym.sinatra.ui.widgets.BackIcon
 import cl.emilym.sinatra.ui.widgets.InfoIcon
 import cl.emilym.sinatra.ui.widgets.JourneyIcon
 import cl.emilym.sinatra.ui.widgets.LocalMapControl
@@ -86,6 +94,7 @@ import sinatra.ui.generated.resources.navigation_bar_favourites
 import sinatra.ui.generated.resources.navigation_bar_map
 import sinatra.ui.generated.resources.navigation_bar_more
 import sinatra.ui.generated.resources.navigation_bar_navigate
+import sinatra.ui.generated.resources.semantics_back_button
 import sinatra.ui.generated.resources.service_alert_title
 
 @Composable
@@ -344,6 +353,22 @@ class RootMapScreen: Screen {
                         bottom = bottomSheetContentPadding
                     )
             ) {
+                val navigator = LocalNavigator.currentOrThrow
+                if (!bottomSheetIsVisible() && isIos && navigator.canPop) {
+                    val backButtonContentDescription = stringResource(Res.string.semantics_back_button)
+                    FloatingActionButton(
+                        onClick = {
+                            navigator.pop()
+                        },
+                        modifier = Modifier
+                            .align(Alignment.TopStart)
+                            .semantics {
+                                contentDescription = backButtonContentDescription
+                            }
+                    ) {
+                        BackIcon()
+                    }
+                }
                 CurrentMapOverlayContent()
             }
         }
