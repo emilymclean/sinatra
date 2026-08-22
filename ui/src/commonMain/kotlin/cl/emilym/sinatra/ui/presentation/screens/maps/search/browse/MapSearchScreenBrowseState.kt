af895355a77eae53a01e9adf5f793fd42390eb32
Emily McLean <emilymcleandeveloper@gmail.com>
2026-08-22 21:26:52 +1000

    Add hint when no routes in area

--- ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/maps/search/browse/MapSearchScreenBrowseState.kt
+++ ui/src/commonMain/kotlin/cl/emilym/sinatra/ui/presentation/screens/maps/search/browse/MapSearchScreenBrowseState.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,12 +27,15 @@ import cl.emilym.sinatra.ui.navigation.LocalBottomSheetState
 import cl.emilym.sinatra.ui.presentation.screens.maps.route.RouteDetailScreen
 import cl.emilym.sinatra.ui.presentation.screens.maps.search.MapSearchViewModel
 import cl.emilym.sinatra.ui.widgets.AlertScaffold
+import cl.emilym.sinatra.ui.widgets.ListHint
+import cl.emilym.sinatra.ui.widgets.NoBusIcon
 import cl.emilym.sinatra.ui.widgets.RouteCard
 import cl.emilym.sinatra.ui.widgets.collectAsStateWithLifecycle
 import cl.emilym.sinatra.ui.widgets.currentLocation
 import org.jetbrains.compose.resources.stringResource
 import sinatra.ui.generated.resources.Res
 import sinatra.ui.generated.resources.browse_routes_in_area
+import sinatra.ui.generated.resources.browse_routes_in_area_no_routes
 
 @OptIn(ExperimentalVoyagerApi::class)
 @Composable
@@ -113,6 +117,14 @@ fun Screen.MapSearchScreenBrowseState(
                                 modifier = Modifier.padding(horizontal = 1.rdp)
                             )
                             Spacer(Modifier.height(0.25.rdp))
+                            if (routes.routes.isEmpty()) {
+                                ListHint(
+                                    stringResource(Res.string.browse_routes_in_area_no_routes),
+                                    modifier = Modifier.padding(horizontal = 1.rdp)
+                                ) {
+                                    NoBusIcon()
+                                }
+                            }
                         }
                     }
                     items(routes.routes.size) {
